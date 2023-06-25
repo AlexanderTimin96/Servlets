50951bc32166a985435a7fab2943e5dd96bb28c6
Timin Alexander <timin.1996@list.ru>
2023-06-25 11:32:17 +0300

    Add homework1(CRUD)

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -9,33 +9,37 @@ import java.io.IOException;
 import java.io.Reader;
 
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    private final Gson gson;
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
+    public PostController(PostService service) {
+        this.service = service;
+        gson = new Gson();
+    }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        final var data = service.all();
+        sendResponceJSON(data, response);
+    }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        final var data = service.getById(id);
+        sendResponceJSON(data, response);
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+        sendResponceJSON(data, response);
+    }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void removeById(long id, HttpServletResponse response) {
+        service.removeById(id);
+    }
+
+    public <T> void sendResponceJSON(T data, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        response.getWriter().print(gson.toJson(data));
+    }
 }
